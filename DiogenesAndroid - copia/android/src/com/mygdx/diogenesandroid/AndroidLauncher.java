c03b46c97321128b8c6a86290878306daf372b83
SaraPdrui <saraPdrui@outlook.com>
2020-03-04 19:29:20 +0100

    Mensaje dialogo introducido

--- DiogenesAndroid - copia/android/src/com/mygdx/diogenesandroid/AndroidLauncher.java
+++ DiogenesAndroid - copia/android/src/com/mygdx/diogenesandroid/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package com.mygdx.diogenesandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,6 +18,33 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new JuegoDiogenesVersionFail(new DatabaseGame(this)), config);
 		startService(new Intent(this, MyService.class));
+
+	}
+
+
+	/**
+	 * Event function activated when the 'Back' Smartphone's button is touched. Exits the game.
+	 * @since March 4th, 2020.
+	 */
+	@Override
+	public void onBackPressed() {
+
+		new AlertDialog.Builder(this)
+				.setTitle("Exit Game")
+				.setMessage("Are you sure you want to close the app?")
+				.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+					/**
+					 * Event function activated when the 'Yes' option from the Dialog is clicked. Exits the game.
+					 * @param dialog The dialog that received the click.
+					 * @param which The button that was clicked.
+					 */
+					@Override
+					public void onClick(DialogInterface dialog, int which) { finish(); }
+
+				})
+				.setNegativeButton("No", null)
+				.show();
 
 	}
 }
